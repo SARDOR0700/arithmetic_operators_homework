d262960619f696c100757bbd407c6753c51185be
SARDOR0700 <sardorjon0700@gmail.com>
2022-12-22 12:04:38 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,8 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the value of the 'answer'
+number = 45 
+x1 = number%10
+x2 = number//10
+print (x1*10+x2)
+answer=54
